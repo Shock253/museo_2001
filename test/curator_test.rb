c7cf35b9c9a024835a17f204f36ee79bf82a2ac9
Ethan Hocking <eshocking253@gmail.com>
2020-03-04 09:38:16 -0700

    Add test for adding photographs

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -5,39 +5,40 @@ require "./lib/curator"
 
 class CuratorTest < Minitest::Test
 
+  def setup
+
+    @photo_1 = Photograph.new({
+         id: "1",
+         name: "Rue Mouffetard, Paris (Boy with Bottles)",
+         artist_id: "1",
+         year: "1954"
+    })
+
+    @photo_2 = Photograph.new({
+         id: "2",
+         name: "Moonrise, Hernandez",
+         artist_id: "2",
+         year: "1941"
+    })
+
+    @curator = Curator.new
+  end
+
   def test_it_exists
-    curator = Curator.new
-    assert_instance_of Curator, curator
+    assert_instance_of Curator, @curator
   end
 
   def test_it_has_atrributes
-    curator = Curator.new
-    assert_equal [], curator.photographs
+    assert_equal [], @curator.photographs
+  end
+
+  def test_can_add_photographs
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+
+    assert_equal [@photo_1, @photo_2], @curator.photographs
   end
 end
-
-# pry(main)> curator = Curator.new
-# # => #<Curator:0x00007fb3d9111cc0...>
-#
-# pry(main)> curator.photographs
-# # => []
-#
-# pry(main)> photo_1 = Photograph.new({
-#      id: "1",
-#      name: "Rue Mouffetard, Paris (Boy with Bottles)",
-#      artist_id: "1",
-#      year: "1954"
-# })
-# # => #<Photograph:0x00007fb3d8ea1cd8...>
-#
-# pry(main)> photo_2 = Photograph.new({
-#      id: "2",
-#      name: "Moonrise, Hernandez",
-#      artist_id: "2",
-#      year: "1941"
-# })
-# # => #<Photograph:0x00007fb3d8e0bfa8...>
-#
 # pry(main)> curator.add_photograph(photo_1)
 #
 # pry(main)> curator.add_photograph(photo_2)
