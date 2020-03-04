7c019b87e6b445e91474e84f8b5f5a1b21243a4d
Ethan Hocking <eshocking253@gmail.com>
2020-03-04 09:23:19 -0700

    Add test for attributes for artist

--- test/artist_test.rb
+++ test/artist_test.rb
@@ -18,21 +18,28 @@ class ArtistTest < Minitest::Test
     assert_instance_of Artist, artist
   end
 
+  def test_it_has_attributes
+    attributes = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    }
+
+    artist = Artist.new(attributes)
+
+
+    assert_equal "2", artist.id
+    assert_equal "Ansel Adams", artist.name
+    assert_equal "1902", artist.born
+    assert_equal "1984", artist.died
+    assert_equal "United States", artist.country
+  end
+
 end
 
 
-
-# pry(main)> attributes = {
-#   id: "2",
-#   name: "Ansel Adams",
-#   born: "1902",
-#   died: "1984",
-#   country: "United States"
-# }
-#
-# pry(main)> artist = Artist.new(attributes)
-# #=> #<Artist:0x00007fc2d0a6c080...>
-#
 # pry(main)> artist.id
 # #=> "2"
 #
