cc2204d92b8a64cf10311fadc6f9ba9845e3210f
Ethan Hocking <eshocking253@gmail.com>
2020-03-04 09:46:50 -0700

    Add add artists test to curator test

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -21,6 +21,23 @@ class CuratorTest < Minitest::Test
          year: "1941"
     })
 
+
+    @artist_1 = Artist.new({
+        id: "1",
+        name: "Henri Cartier-Bresson",
+        born: "1908",
+        died: "2004",
+        country: "France"
+    })
+
+    @artist_2 = Artist.new({
+        id: "2",
+        name: "Ansel Adams",
+        born: "1902",
+        died: "1984",
+        country: "United States"
+    })
+
     @curator = Curator.new
   end
 
@@ -30,6 +47,7 @@ class CuratorTest < Minitest::Test
 
   def test_it_has_atrributes
     assert_equal [], @curator.photographs
+    assert_equal [], @curator.artists
   end
 
   def test_can_add_photographs
@@ -38,41 +56,14 @@ class CuratorTest < Minitest::Test
 
     assert_equal [@photo_1, @photo_2], @curator.photographs
   end
+
+  def test_can_add_artists
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+
+    assert_equal [@artist_1, @artist_2], @curator.artists
+  end
 end
-# pry(main)> curator.add_photograph(photo_1)
-#
-# pry(main)> curator.add_photograph(photo_2)
-#
-# pry(main)> curator.photographs
-# # => [#<Photograph:0x00007fb3d8ea1cd8...>, #<Photograph:0x00007fb3d8e0bfa8...>]
-#
-# pry(main)> curator.artists
-# # => []
-#
-# pry(main)> artist_1 = Artist.new({
-#     id: "1",
-#     name: "Henri Cartier-Bresson",
-#     born: "1908",
-#     died: "2004",
-#     country: "France"
-# })
-# # => #<Artist:0x00007fb3d8b4ed60...>
-#
-# pry(main)> artist_2 = Artist.new({
-#     id: "2",
-#     name: "Ansel Adams",
-#     born: "1902",
-#     died: "1984",
-#     country: "United States"
-# })
-# # => #<Artist:0x00007fb3d90bb4b0...>
-#
-# pry(main)> curator.add_artist(artist_1)
-#
-# pry(main)> curator.add_artist(artist_2)
-#
-# pry(main)> curator.artists
-# # => [#<Artist:0x00007fb3d8b4ed60...>, #<Artist:0x00007fb3d90bb4b0...>]
-#
+
 # pry(main)> curator.find_artist_by_id("1")
 # # => #<Artist:0x00007fb3d8b4ed60...>
